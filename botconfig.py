9a611a9436121a2a435841603abcacdd5ca0a66e
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2020-02-04 20:04:52 +0200

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,4 +1,12 @@
 #Конфигурация для бота
 PREFIX_COMMAND = '$' # Префикс для команды
 
+server_ip = '188.246.224.82:25700'
+server_version = '1.14.4'
+
+donate_qiwi = '+79162421415'
+donate_webmoney = 'None'
+
+BOT_AUTHOR = "Lonely_#1572"
+
 BOT_STATUS = "Minecraft."
