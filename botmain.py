4937fddfbe98645d9cfcc913928d31ac12985077
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2020-02-04 19:10:00 +0200

    Update botmain.py

--- botmain.py
+++ botmain.py
@@ -5,22 +5,37 @@ import os
 
 bot = commands.Bot(command_prefix=PREFIX_COMMAND)
 
+bot.remove_command('help') # Удаляет команду help
+
 @bot.event
 async def on_ready():
-    print('------')
-    print("      ")
-    print('Бот успешно запущен')
-    print("Имя бота: "+ bot.user.name)
-    print("      ")
-    print('------')
-
-@bot.command(pass_context=True)  # разрешаем передавать агрументы
-async def тест(ctx, arg):  # создаем асинхронную фунцию бота
-    await ctx.send(arg)  # отправляем обратно аргумент
+    activity = activity = discord.Game(name=BOT_STATUS) # Задает статус, берет их botconfig.py
+    await bot.change_presence(status=discord.Status.idle, activity=activity) # Применяет статус
+    print('Бот запущен')
 
 @bot.command()
-async def Инфо(ctx):
-    await ctx.send(MESSAGE_INFO)
+async def say(ctx):
+    say_at_me = input("Введите сообщение через консоль: ")
+    await ctx.send(say_at_me)
+
+@bot.command()
+async def Помощь(ctx):
+    embed = discord.Embed(title="Все команды которые может предоставить **Енот Бот**", description="", color=0xeee657)
+    embed.add_field(name='$Сервер', value="Информация о сервере.", inline=False)
+    embed.set_footer(text="Все права на бота пренадлежат: Lonely_#1572.") # Подвал сообщения
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def Сервер(ctx):
+    embed = discord.Embed(title="Сервер: **Legend of The World**", description="**Енот Бот** был сделан специально для этого сервера.", color=0xeee657)
+    embed.add_field(name=":wave: **Привет дорогой друг.** :wave:", value="Если ты тут значит тебя приняли,\n чтобы узнать айпи напиши - $ip", inline=True)
+    embed.add_field(name="**Немного о сервере:**", value="Нету приватов, нету доната, свобода действий, не ограниченая территория.", inline=False)
+    embed.add_field(name="**Узнать все команды:**", value="$Помощь.", inline=False)
+    embed.add_field(name="**Пожертвования:**", value="Если у вас появилось желание помочь серверу\n просто напишите - $donate", inline=False)
+    embed.set_footer(text="Все права на бота пренадлежат: **Lonely_#1572**") # Подвал сообщения
+
+    await ctx.send(embed=embed)
+
 
 @bot.command()
 async def cat(ctx):
