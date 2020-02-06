04b246ab1b5dcc621aafee27218891b3e2c25b09
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2020-02-06 10:03:33 +0200

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -4,7 +4,7 @@ PREFIX_COMMAND = '$' # Префикс для команды
 server_ip = '188.246.224.82:25700' #  IP-Адрес сервера
 server_version = '1.14.4' # Версия сервера
 
-donate_qiwi = '+79162421415' # Реквизит киви
+donate_qiwi = 'None' # Реквизит киви
 donate_webmoney = 'None' # Реквизит Веб Мани
 
 version = "1.1.0.0" # Версия
