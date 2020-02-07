8c8627ba2767af27c7e8238a14b2657d0588857c
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2020-02-07 19:39:24 +0200

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,16 +1,27 @@
 #Конфигурация для бота
+
+# Setting
 PREFIX_COMMAND = '.' # Префикс для команды
 
-BOT_STATUS = "Minecraft"
+# First Start
+BOT_STATUS = "Тестовый  режим"
+channel_start_message = '672163621299552349'
 
+# join 
+chennel_join_message = '654042717382246420'
+
+# ip
 server_ip = '188.246.224.82:25700'
 server_version = '1.14.4'
 
-donate_qiwi = 'None'
+# donate
+donate_qiwi = 'None' # +79162421415
 donate_webmoney = 'None'
 
-channel_hello_bot = '647180605573955604'
-help_message_hello = 'Помощь'
+# Join message
+ip_private_message_onejoin = 'ip'
+help_private_message_onejoin = 'Помощь'
 
-version = "1.1.2.0"
+# ver
+version = "Тестовая 1.1.2.0" # 1.1.2.0
 BOT_AUTHOR = "Lonely_#1572"
