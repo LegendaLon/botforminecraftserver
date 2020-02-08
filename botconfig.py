06cbea7ce90f1d6adacb465b0cb28b5bce232336
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2020-02-08 15:35:00 +0200

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,14 +1,19 @@
-#Конфигурация для бота
+#Конфигурация для Бота.
 
 # Setting
 PREFIX_COMMAND = '.' # Префикс для команды
 
 # First Start
-BOT_STATUS = "Minecraft"
-channel_start_bot_message = '672163621299552349'
+BOT_STATUS = "Тестовый  режим"
+channel_start_bot_message = 672163621299552349
 
 # join 
-chennel_join_message = '654042717382246420'
+chennel_join_message = 654042717382246420
+
+# Code
+
+code1 = '1830962036'
+code1_comment = 'Тестовый код  для получения VIP'
 
 # ip
 server_ip = '188.246.224.82:25700'
@@ -23,5 +28,5 @@ ip_private_message_onejoin = 'ip'
 help_private_message_onejoin = 'Помощь'
 
 # ver
-version = "Основная 1.1.2.0" # 1.1.2.0
+version = "Основная 1.1.3.0" # 1.1.2.0
 BOT_AUTHOR = "Lonely_#1572"
