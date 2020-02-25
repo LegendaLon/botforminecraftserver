6c8fcf474b6c7603b0223c2775deecabd6349e81
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2020-02-25 22:13:57 +0200

    Update botmain.py

--- botmain.py
+++ botmain.py
@@ -219,48 +219,6 @@ async def code(ctx, arg1, amount=1): # Создает команду
     else:
         await author.send(embed=discord.Embed(description='Вы ввели не существующий код!!', color=red), delete_after=300)
 
-# Request Accept and Refuse(Отказать)
-
-# @client.command(aliases = ["request", "Заявка", "заявка"])
-# async def Request(ctx, *, arg):
-#    author = ctx.message.author
-#    channel = ctx.message.channel
-#    channel_white = client.get_channel(botconfig.channel_request_white)
-#    channel_admin = client.get_channel(botconfig.channel_admin)
-#    if channel == channel_white:
-#        await ctx.send(embed=discord.Embed(description=f'{author.name}, заявка принята, ожидайте!', color=orange), delete_after=60)
-#        embed = discord.Embed(title='Новая заявка!', color=orange)
-#        embed.add_field(name='Автор:', value=f'Заявку написал {author}.', inline=False)
-#        embed.add_field(name='Вот заявка', value=f'``{arg}``', inline=False)
-#        embed.add_field(name='Принять', value=f'Чтоб принять заявку напишите: ``{botconfig.PREFIX_COMMAND}принять [Упоминание участника]`` в #заявка', inline=True )
-#        embed.add_field(name='Отказать', value=f'Чтоб отказать заявку напишите: ``{botconfig.PREFIX_COMMAND}отказать [Упоминание участника]`` в #заявка', inline=True )
-#        embed.set_footer(text=f"Все права на бота пренадлежат: {botconfig.BOT_AUTHOR}") # Подвал сообщения#
-# 
-#         await channel_admin.send(embed=embed) # В модераторскую
-#     else:
-#         await ctx.send(embed=discord.Embed(description=f'{author.name}, заявку нельзя писать в этот чат!', color=red), delete_after=60) 
-# 
-# @client.command(aliases = ["accept", "Принять", "принять"])
-# @commands.has_permissions(manage_roles=True)
-# async def Accept(ctx, member: discord.Member):
-#     role_add = utils.get(member.guild.roles, id=botconfig.roll_add_accept)
-#     role_rem = utils.get(member.guild.roles, id=botconfig.roll_add)
-#     channel = client.get_channel(botconfig.channel_start_bot_message) 
-#     print(f'Принят {member}')
-#     await member.add_roles(role_add)
-#     await member.remove_roles(role_rem)
-#     await channel.send(embed=discord.Embed(description=f'{member.name}, был принят! :tada: :tada:', color=orange))
-# 
-#@client.command(aliases = ["denny", "Отказать", "отказать"])
-#@commands.has_permissions(manage_roles=True)
-#async def Denny(ctx, member: discord.Member):
-#    await member.send(embed=discord.Embed(description=f':wave: Привет {member.name} тебя не приняли :frowning2: :frowning2: , попробуй ещё ', color=orange))
-#
-#@client.command(aliases = ["заявка_помощь", "Request_help", "request_help"])
-#async def Заявка_помощь(ctx):
-#    author = ctx.message.author
-#    channel = client.get_channel(botconfig.channel_request)
-#    await ctx.send(f'{author.mention}, заявку можна найти в {channel.mention}!', delete_after=120)
 
 # Error
 
@@ -273,30 +231,6 @@ async def add_event_error(ctx, error, amount = 1):
 @list_event.error
 async def list_event_error(ctx, error, amount = 1):
     if isinstance(error, commands.errors.MissingPermissions):
-        await ctx.channel.purge(limit=amount)
-        await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, у вас нету прав чтоб использывать эту функцию!', color=red), delete_after=60)
-
-@Request.error
-async def Request_error(ctx, error, amount = 1):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.channel.purge(limit=amount)
-        await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, вы не написали заявку!! Чтобы узнать вопросы заявки напишите ``.заявка_помощь``', color=red), delete_after=60)
-
-@Accept.error
-async def Accept_error(ctx, error, amount = 1):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.channel.purge(limit=amount)
-        await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, вы не указали участника!! ``.принять [Упоминание участника]``', color=red), delete_after=60)
-    elif isinstance(error, commands.errors.MissingPermissions):
-        await ctx.channel.purge(limit=amount)
-        await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, у вас нету прав чтоб использывать эту функцию!', color=red), delete_after=60)
-
-@Denny.error
-async def Denny_error(ctx, error, amount = 1):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.channel.purge(limit=amount)
-        await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, вы не указали участника!! ``.принять [Упоминание участника]``', color=red), delete_after=60)
-    elif isinstance(error, commands.errors.MissingPermissions):
         await ctx.channel.purge(limit=amount)
         await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, у вас нету прав чтоб использывать эту функцию!', color=red), delete_after=60)
 
