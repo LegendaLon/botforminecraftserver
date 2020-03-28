bace4e3b5f61fc3d37f37f7ea8c4e8522bf9f438
LegendaLon <LegendaLon@users.noreply.github.com>
2020-03-28 20:01:09 +0200

    new udpate

--- botmain.py
+++ botmain.py
@@ -23,27 +23,6 @@ client = commands.Bot(command_prefix=botconfig.PREFIX_COMMAND) # Префикс 
 
 client.remove_command('help') # Удаляет команду help
 
-# Error
-# @add_event.error
-# async def add_event_error(ctx, error, amount = 1):
-#     if isinstance(error, commands.errors.MissingRequiredArgument):
-#         await ctx.channel.purge(limit=amount)
-#         await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, вы не ввели свой ник!! ``.add_event [Ваш ник]``', color=red), delete_after=60)
-
-# @list_event.error
-# async def list_event_error(ctx, error, amount = 1):
-#     if isinstance(error, commands.errors.MissingPermissions):
-#         await ctx.channel.purge(limit=amount)
-#         await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, у вас нету прав чтоб использывать эту функцию!', color=red), delete_after=60)
-
-""" Join Cogs """
-# client.load_extension('cogs.EventGiveRoles') # Выдача ролей
-# client.load_extension('cogs.EventGlobal') # Глобальные ивенты
-# client.load_extension('cogs.CommandHelp') # Команды помощи
-# client.load_extension('cogs.CommandGame') # Игровые команды
-# client.load_extension('cogs.CommandUser') # Пользовательские команды
-
-""" RUN """  
 # client.event
 
 @client.event
@@ -277,6 +256,5 @@ async def list_event_error(ctx, error, amount = 1):
         await ctx.send(embed=discord.Embed(description=f'{ctx.author.name}, у вас нету прав чтоб использывать эту функцию!', color=red), delete_after=60)
 
 # RUN
-token = os.environ.get('BOT_TOKEN')
-token = 'NjcxMDExODk4NzE1NjY4NDgx.XnqqyA.Tm3he2uvhfKUgz4vsGuw1YPwcuM'
-client.run(str(token))
+bot_token = 'NjcxMDExODk4NzE1NjY4NDgx.Xn-QgQ.qu9uP0SFCkP2BKeXoxby087x9hw'
+client.run(str(bot_token))
